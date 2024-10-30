44e92d8bd7d57870428b097c504b6d53d3df364f
vern <vernhaou.liew@double.my>
2024-10-30 12:41:48 +0800

    chore: fixed typo

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -52,7 +52,7 @@ const Footer = ({
             {renderUser()}
 
             <div>
-                T Insurance Customer Portal
+                Z Insurance Customer Portal
             </div>
         </div>
     );
